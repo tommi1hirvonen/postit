c23b804939554e8eb301ab318fa119bf7b3f046d
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2023-09-30 19:47:11 +0300

    Added endpoint for getting user by id

--- Api/Functions/Users.cs
+++ Api/Functions/Users.cs
@@ -16,14 +16,28 @@ public class Users
     }
 
     [Function("Users")]
-    public async Task<HttpResponseData> Run(
-        [HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
+    public async Task<HttpResponseData> GetUsersAsync(
+        [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = "Users")] HttpRequestData req,
         [CosmosDBInput("Postit", "Users",
             Connection = "CosmosDBConnectionString")] IEnumerable<User> users)
     {
         _logger.LogInformation("Fetching users");
         var response = req.CreateResponse(HttpStatusCode.OK);
         await response.WriteAsJsonAsync(users);
+        return response;
+    }
+
+    [Function("User")]
+    public async Task<HttpResponseData> GetUserByIdAsync(
+        [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = "Users/{userId}")] HttpRequestData req,
+        [CosmosDBInput("Postit", "Users",
+            Connection = "CosmosDBConnectionString",
+            Id = "{userId}",
+            PartitionKey = "{userId}")] User user)
+    {
+        _logger.LogInformation("Fetching users");
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        await response.WriteAsJsonAsync(user);
         return response;
     }
 }
