4bf702636e74cae636cb7711defa025518feaa8f
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2024-01-01 15:54:22 +0200

    Fix API after net8.0 upgrade

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,8 +1,15 @@
 global using Postit.Models;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
-    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureFunctionsWebApplication()
+    .ConfigureServices(services => 
+    {
+        services.AddApplicationInsightsTelemetryWorkerService();
+        services.ConfigureFunctionsApplicationInsights();
+    })
     .Build();
 
 host.Run();
